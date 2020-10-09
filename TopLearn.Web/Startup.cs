79cdcbbeac90366148c31b17686de35238439f48
Masoud Khodadadi <masoud.xpress@gmail.com>
2020-10-10 01:21:18 +0330

    Add admin layout

--- TopLearn.Web/Startup.cs
+++ TopLearn.Web/Startup.cs
@@ -81,6 +81,8 @@ namespace TopLearn.Web
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapRazorPages();
             });
         }
     }
